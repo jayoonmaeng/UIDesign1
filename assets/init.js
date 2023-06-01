27435c3003e668e240f043ed58154472939f55b8
jayoonmaeng <jayun951@gmail.com>
2023-06-01 14:08:01 +0900

    first june

--- assets/init.js
+++ assets/init.js
@@ -6,9 +6,8 @@ ready for the code inside of the
 function to be executed 
 */
 
-$(document).ready(function() {
-    
-    /* 
+$(document).ready(function () {
+  /* 
     jquery follows its own syntax. it uses
     the $ symbol to look for an element. 
     followed by () which is the element to find
@@ -18,35 +17,43 @@ $(document).ready(function() {
     inside the curly brackets.
     */
 
-    $('body').click(function(){
-        $(this).css('background', 'yellow');
-    })
+  //   $("body").click(function () {
+  //     $(this).css("background", "yellow");
+  //   });
 
-    /* random background color from an array */
+  /* random background color from an array */
 
-    // let colors = ['blue', 'red', 'green', 'purple', 'yellow']
-    // $('body').click(function(){
-    //     var color = colors[Math.floor(Math.random() * colors.length)];
-    //     $(this).css('background', color);
-    // });
+  // let colors = ['blue', 'red', 'green', 'purple', 'yellow']
+  // $('body').click(function(){
+  //     var color = colors[Math.floor(Math.random() * colors.length)];
+  //     $(this).css('background', color);
+  // });
 
-    /* add a character to a list */
+  /* add a character to a list */
 
-    // $('li').mouseover(function(){
-    //     $(this).append('👋');
-    // });
+  // $('li').mouseover(function(){
+  //     $(this).append('👋');
+  // });
 
-    /* make a bug follow your cursor and resize */
+  /* make a bug follow your cursor and resize */
 
-    // $('body').append('<div id="follow">🦟</div>');
-    // $(document).on("mousemove", function (event) {
-    //     $('#follow').css({
-    //         'font-size' : event.pageY/10+'px',
-    //         'position' : 'absolute',
-    //         'left' : event.pageX+10+'px',
-    //         'top' : event.pageY+10+'px'
-    //     });
-    // });
-
-
+  // $('body').append('<div id="follow">🦟</div>');
+  // $(document).on("mousemove", function (event) {
+  //     $('#follow').css({
+  //         'font-size' : event.pageY/10+'px',
+  //         'position' : 'absolute',
+  //         'left' : event.pageX+10+'px',
+  //         'top' : event.pageY+10+'px'
+  //     });
+  // });
+  $(".cell button").on("click", function () {
+    var num = parseInt($(".img-container").attr("img_num"));
+    num++;
+    console.log(num);
+    if (num > 4) {
+      num = 1;
+    }
+    $(".img-container").attr("src", "assets/img/" + num + ".png");
+    $(".img-container").attr("img_num", num);
+  });
 });
